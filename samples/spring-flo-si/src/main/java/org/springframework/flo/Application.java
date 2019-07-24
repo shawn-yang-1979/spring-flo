00d8975675c0ecdf5937a2ca5a9f870349cf6a96
SHAWN.SH.YANG <SHAWN.SH.YANG@TWTP1NB1024.delta.corp>
2019-07-24 18:45:56 +0800

    some linting error

--- samples/spring-flo-si/src/main/java/org/springframework/flo/Application.java
+++ samples/spring-flo-si/src/main/java/org/springframework/flo/Application.java
@@ -17,10 +17,6 @@ package org.springframework.flo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * 
@@ -28,19 +24,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  * @author Alex Boyko
  */
 @SpringBootApplication
-public class Application { 
+public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
-
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurerAdapter() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/").allowedOrigins("http://localhost:9000");
-//            }
-//        };
-//    }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 }
